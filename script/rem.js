cbd92afbd5f788a3dde130c258fc2634b1e0b4ff
wyglove <53166997+wyglove@users.noreply.github.com>
2019-12-30 20:13:35 +0800

    删除
    删除

--- script/rem.js
+++
@@ -1,6 +0,0 @@
-(function(w) {
-    w = w || 375;
-    var sw = document.documentElement.clientWidth;
-    sw = sw > 320 ? sw : 320;
-    document.getElementsByTagName('html')[0].style.fontSize = Math.floor(sw/(w || 375) * 100) +'px';
-})(375);
--- script/root.js
+++
@@ -1,5 +0,0 @@
-function htmlLoad(){
-    //set('DD_winName',[]);
-    api.pageParam.curPageBarColor='light';
-    openWin({'mod':'root','act':'root','animation':{'type':'none','duration':10},'slidBackEnabled':false});
-}
